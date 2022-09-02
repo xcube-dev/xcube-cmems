c775c53134e5c38f361dd4975452b133838343bd
tejas <tejas.morbagalharish@brockmann-consult.de>
2022-09-02 16:43:06 +0200

    refactoring

--- xcube_cmems/cmems.py
+++ xcube_cmems/cmems.py
@@ -72,7 +72,7 @@ class Cmems:
         return urls
 
     @staticmethod
-    def get_csw_records(csw, pagesize=10, max_records=300) -> Dict[Any]:
+    def get_csw_records(csw, pagesize=10, max_records=300) -> Dict[Any, Any]:
         """
         Iterate max_records/pagesize times until the requested value in
         max_records is reached.
