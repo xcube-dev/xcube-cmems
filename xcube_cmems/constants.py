73e105406ba48dafa9cc87bb61c3a3ff3c4081ad
tejas <tejas.morbagalharish@brockmann-consult.de>
2022-09-26 15:45:01 +0200

    adjusted the get_var_descriptors and describe_data methods to validate the schema and added get_data_store_params_schema method

--- xcube_cmems/constants.py
+++ xcube_cmems/constants.py
@@ -30,4 +30,3 @@ CSW_URL = "https://cmems-catalog-ro.cls.fr/geonetwork/srv/eng/csw-MYOCEAN" \
           "-CORE-PRODUCTS?"
 DATA_STORE_ID = 'cmems'
 DATASET_OPENER_ID = f'dataset:zarr:{DATA_STORE_ID}'
-ZARR_DATA_STORE_ID = 'cmemszarr'
