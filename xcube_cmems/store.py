2169a46fa8a1bfc0046cc2d53d44c41a1869f682
tejas <tejas.morbagalharish@brockmann-consult.de>
2024-03-22 10:10:22 +0100

    refactor

--- xcube_cmems/store.py
+++ xcube_cmems/store.py
@@ -29,12 +29,25 @@ import copernicusmarine as cm
 
 from xarray.core.dataset import DataVariables
 from xcube.core.gridmapping import GridMapping
-from xcube.core.store import (DATASET_TYPE, DataDescriptor, DataOpener, DataStore,
-                              DataStoreError, DataType, DataTypeLike, DatasetDescriptor,
-                              VariableDescriptor)
+from xcube.core.store import (
+    DATASET_TYPE,
+    DataDescriptor,
+    DataOpener,
+    DataStore,
+    DataStoreError,
+    DataType,
+    DataTypeLike,
+    DatasetDescriptor,
+    VariableDescriptor,
+)
 from xcube.util.assertions import assert_not_none
-from xcube.util.jsonschema import (JsonArraySchema, JsonDateSchema, JsonNumberSchema,
-                                   JsonObjectSchema, JsonStringSchema)
+from xcube.util.jsonschema import (
+    JsonArraySchema,
+    JsonDateSchema,
+    JsonNumberSchema,
+    JsonObjectSchema,
+    JsonStringSchema,
+)
 
 from .cmems import Cmems
 from .constants import DATASET_OPENER_ID
